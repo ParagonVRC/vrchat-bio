1c0862c498ead3cc0ac82994af7715cf8a7f971b
cloak <frederikwer@gmail.com>
2025-01-17 19:50:55 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('contactForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    alert(`Thank you, ${name}! we'll contact you at ${email}`);
+});
